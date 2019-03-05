1f3c4f961011098be4e42221a29c4df0af763381
Jerry <jerry.gu@9client.com>
2019-03-05 17:08:56 +0800

    tag4.0

--- JKIMFramework.podspec
+++ JKIMFramework.podspec
@@ -88,7 +88,7 @@ Pod::Spec.new do |s|
   #  For header files it will include any header in the folder.
   #  Not including the public_header_files will make all headers public.
   #
-   s.source_files  = "JKIMFrameworkTest/*.a"
+   s.source_files  = "JKIMTest/*"
   #s.exclude_files = "Classes/Exclude"
 
   # s.public_header_files = "Classes/**/*.h"
---
+++ JKIMTest/JKIMFrameWork.framework/Headers/JKIMFrameWork.h
@@ -0,0 +1,19 @@
+//
+//  JKIMFrameWork.h
+//  JKIMFrameWork
+//
+//  Created by Jerry on 2019/3/5.
+//  Copyright © 2019 于飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JKIMFrameWork.
+FOUNDATION_EXPORT double JKIMFrameWorkVersionNumber;
+
+//! Project version string for JKIMFrameWork.
+FOUNDATION_EXPORT const unsigned char JKIMFrameWorkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JKIMFrameWork/PublicHeader.h>
+#import <JKIMFrameWork/TestObject.h>
+
