06f72448307eb3d400882f7a1b4f9fd409d492f0
tiandong <tiandong.chen>
2022-11-28 11:53:55 +0800

    1.9.6

---
+++ JKIMFramework/JKIMFramework/Classes/JKNetWorkTipCell.h
@@ -0,0 +1,21 @@
+//
+//  JKNetWorkTipCell.h
+//  JKIMSDKProject
+//
+//  Created by 陈天栋 on 2022/11/24.
+//  Copyright © 2022 于飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "JKMessageFrame.h"
+
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface JKNetWorkTipCell : UITableViewCell
+
+@property (nonatomic, strong)JKMessageFrame *messageFrame;
+
+@end
+
+NS_ASSUME_NONNULL_END
